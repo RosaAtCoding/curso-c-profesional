d1062bd59bfafa4018d685a7d4766b19015b459a
Rosa Mo <rosaatcoding@gmail.com>
2026-05-15 21:05:32 +0200

    Estructura básica de un programa en C

---
+++ Bloque_2/seccion_2_1.c
@@ -0,0 +1,14 @@
+/*
+Sección 2.1 - Estructura básica de un programa en C
+
+Objetivo: Entender las piezas mínimas de un prorama: includes y main()
+*/
+
+#include <stdio.h>  // Librería estándar de entrada/salida (printf, scanf, ...)
+
+int main(void) {    // Punto de entrada del programa. "void" indica que no recibe argumentos
+
+    printf("Esta es la estructura básica de un programa en C.\n");
+
+    return 0;   // "0" normalmente significa "Exito" para el Sistema Operativo
+}
