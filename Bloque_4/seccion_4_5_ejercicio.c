ae26a9d2b6d2606c3e9590c6e5eda73b48ddb627
Rosa Mo <rosaatcoding@gmail.com>
2026-06-07 13:34:42 +0200

    4.5. Ejercicio propuesto - Solución

--- Bloque_4/seccion_4_5_ejercicio.c
+++ Bloque_4/seccion_4_5_ejercicio.c
@@ -18,3 +18,49 @@ Notas:
 - Asume datos válidos
 */
 
+#include <stdio.h>
+
+int potenciaIterativa(int base, int exponente);
+int potenciaRecursiva(int base, int exponente);
+
+int main(void) {
+    int base;
+    int exponente;
+
+    printf("Base (ingresa un número entero (>0)): ");
+    scanf("%d", &base);
+
+    printf("Exponente: (ingresa un número entero (>0)): ");
+    scanf("%d", &exponente);
+
+    // Asumimos entrada válida
+    int resultado1 = potenciaIterativa(base, exponente);
+    int resultado2 = potenciaRecursiva(base, exponente);
+
+    printf("Potencia iterativa %d^%d = %d\n", base, exponente, resultado1);
+    printf("Potencia recursiva %d^%d = %d\n", base, exponente, resultado2);
+
+    return 0;
+}
+
+int potenciaIterativa(int base, int exponente) {
+    int potencia = 1;
+    int i;
+
+    for (i = 1; i <= exponente; i++) {
+        potencia *= base;
+    }
+
+    return potencia;
+}
+
+int potenciaRecursiva(int base, int exponente) {
+    // Caso base
+    if (exponente <= 0) {
+        return 1;   // potenciaRecursiva(base, 0) = 1
+    }
+
+    // Paso recursivo
+    return base * potenciaRecursiva(base, exponente - 1);
+}
+
